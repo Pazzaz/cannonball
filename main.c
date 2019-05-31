584abc276f96c30932e6f0c5239dc6cc5dcffd06
Pazzaz <pazzaz.sundqvist@gmail.com>
2019-05-31 19:50:51 +0200

    Make (somewhta faster) faster and clener

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(void)
     mpz_t Sm4tSm4;
     mpz_t Sm2t8;
     mpz_t Sm2t2;
-    mpz_init_set_str(big, "100000", 10);
+    mpz_init_set_str(big, "10000", 10);
     mpz_init(Sbig);
     mpz_init(pns);
     mpz_init(sum);
@@ -33,33 +33,36 @@ int main(void)
     mpz_init(Sm4);
     mpz_init(Sm4tSm4);
     mpz_init(Sm2t2);
-    mpz_set_str(Sbig, "5", 10);
+    mpz_set_str(Sbig, "109790245", 10);
 
     for(;;mpz_add_ui(Sbig, Sbig, 1)) {
-        mpz_set_str(pns, "1", 10);
-        mpz_set_str(sum, "1", 10);
+        mpz_set_ui(pns, 8);
+        mpz_set_ui(sum, 1);
         mpz_sub_ui(Sm2, Sbig, 2);
+        // Skip when there is a parametric solution: S = 3n+2
+        if (mpz_divisible_ui_p(Sm2, 3)) continue;
         mpz_mul_ui(Sm2t8, Sm2, 8);
         mpz_mul(sum, sum, Sm2t8);
         mpz_set_ui(n, 1);
+        mpz_set_ui(tmp1, 8);
         mpz_sub_ui(Sm4, Sbig, 4);
         mpz_mul(Sm4tSm4, Sm4, Sm4);
         mpz_mul_ui(Sm2t2, Sm2, 2);
         for(;mpz_cmp(n, big); mpz_add_ui(n, n, 1)) {
-            // Skip when we have solutions: S = 3n+2
-            if (mpz_divisible_ui_p(Sm2, 3)) break;
-            // pns = n(S-2)+1+pns
-            mpz_mul(tmp1, n, Sm2);
-            mpz_add_ui(tmp1, tmp1, 1);
+            // 8*pns = n(S-2)*8+8+pns*8
+            mpz_add(tmp1, tmp1, Sm2t8);
             mpz_add(pns, tmp1, pns);
 
             // sum = sum+pns*8*(S-2)
-            mpz_mul(tmp2, pns, Sm2t8);
+            mpz_mul(tmp2, pns, Sm2);
             mpz_add(sum, sum, tmp2);
 
+            // tmp2 = sum + (S-4)^2
             mpz_add(tmp2, sum, Sm4tSm4);
 
             if(mpz_perfect_square_p(tmp2)) {
+                mpz_get_str(str1, 10, Sbig);
+                printf("current progress: %s\n", str1);
                 mpz_sqrt(tmp2, tmp2);
                 mpz_add(tmp2, tmp2, Sm4);
                 if(mpz_divisible_p(tmp2, Sm2t2)) {
