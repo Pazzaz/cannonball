1c1e0db4aa60ecda1675b7a55ddd57d531d3108b
Pazzaz <pazzaz.sundqvist@gmail.com>
2019-06-01 15:38:47 +0200

    Cleanup; use long.

--- main2.c
+++ main2.c
@@ -10,41 +10,22 @@ int main(void)
     char str2[100];
     char str3[100];
     char str4[100];
-    mpz_t n;
-    mpz_t Sbig;
     mpz_t tmp1;
     mpz_t tmp2;
     mpz_t tmp3;
-    mpz_t sum;
-    mpz_t big;
-    mpz_t pns;
-    mpz_t Sm2;
-    mpz_t Sm4;
-    mpz_t Sm4tSm4;
-    mpz_t Sm2t8;
-    mpz_t Sm2t2;
-    mpz_init_set_str(big, "10000", 10);
-    mpz_init(Sbig);
-    mpz_init(pns);
-    mpz_init(sum);
-    mpz_init(n);
     mpz_init(tmp1);
     mpz_init(tmp2);
     mpz_init(tmp3);
-    mpz_init(Sm2t8);
-    mpz_init(Sm2);
-    mpz_init(Sm4);
-    mpz_init(Sm4tSm4);
-    mpz_init(Sm2t2);
-    mpz_set_str(Sbig, "6", 10);
+    unsigned long S = 5;
+    unsigned long Sm2 = S-2;
+    unsigned long Sm4 = S-4;
 
-    int array_size = 100000;
+    int array_size = 1000;
+    printf("array size: %d\n", array_size);
     mpz_t *large;
     large = malloc(array_size * sizeof(mpz_t));
-    mpz_t *addition;
-    addition = malloc(array_size * sizeof(mpz_t));
-    mpz_t *addition2;
-    addition2 = malloc(array_size * sizeof(mpz_t));
+    unsigned long addition[array_size];
+    unsigned long addition2[array_size];
 
     // Starting with s = 5
     for(unsigned long i = 0;i < array_size; i++)
@@ -55,60 +36,55 @@ int main(void)
 
     for(unsigned long i = 0;i < array_size; i++)
     {   
-        mpz_init_set_ui(addition[i], 4*(( (i*i*i)-i)/3));
+        addition[i] = 4*(( (i*i*i)-i)/3);
     }
 
     for(unsigned long i = 0;i < array_size; i++)
     {   
-        mpz_init_set_ui(addition2[i], 8*(( (i*i*i)-i)/3));
+        addition2[i] = 8*(( (i*i*i)-i)/3);
     }
-    // for(unsigned long i = 0;i < array_size; i++)
-    // {   
-    //     mpz_get_str(str1, 10, large[i]);
-    //     printf("large before: %s\n", str1);
-    // }
-    printf("haha\n");
-    for(char mod = 1;;mpz_add_ui(Sbig, Sbig, 1), mod++) {
-        if(mod % 3 == 0) {
-            mpz_add_ui(Sbig, Sbig, 1);
+    for(char mod = 0;; mod++) {
+        char skip = 0;
+        if (mod == 2) {
+            S += 2;
+            Sm2 += 2;
+            Sm4 += 2;
+            mod = 0;
+            skip = 1;
+        } else {
+            S += 1;
+            Sm2 += 1;
+            Sm4 += 1;
         }
-        mpz_sub_ui(Sm2, Sbig, 2);
-       
-
-        mpz_sub_ui(Sm4, Sbig, 4);
-        mpz_mul(Sm4tSm4, Sm4, Sm4);
+        if (S>>32) {
+            return 1;
+        }
+        unsigned long Sm4tSm4 = Sm4*Sm4;
 
         for (unsigned long j = 2; j < array_size; j++)
         {   
-            if(mod % 3 == 0) {
-                mpz_add(large[j], large[j], addition2[j]);
+            if (skip) {
+                mpz_add_ui(large[j], large[j], addition2[j]);
             } else {
-                mpz_add(large[j], large[j], addition[j]);
+                mpz_add_ui(large[j], large[j], addition[j]);
             }
-
-            mpz_mul(tmp1, large[j], Sm2);
-            mpz_add(tmp1, tmp1, Sm4tSm4);
+        
+            mpz_mul_ui(tmp1, large[j], Sm2);
+            mpz_add_ui(tmp1, tmp1, Sm4tSm4);
 
             if(mpz_perfect_square_p(tmp1)) {
                 mpz_sqrt(tmp2, tmp1);
-                mpz_add(tmp2, tmp2, Sm4);
+                mpz_add_ui(tmp2, tmp2, Sm4);
 
-                mpz_mul_ui(Sm2t2, Sm2, 2);
-                if(mpz_divisible_p(tmp2, Sm2t2)) {
+                printf("current progress: %ld\n", S);
+                unsigned long Sm2t2 = Sm2*2;
+                if(mpz_divisible_ui_p(tmp2, Sm2t2)) {
                     mpz_divexact_ui(tmp2, large[j], 8);
                     mpz_get_str(str2, 10, tmp2);
-                    mpz_get_str(str3, 10, Sbig);
-                    if (mpz_divisible_ui_p(Sm2, 3)) continue;
-                    printf("sides: %s, height: %ld, count: %s\n", str3, j, str2);
+                    printf("sides: %ld, height: %ld, count: %s\n", S, j, str2);
                 }
             }
         }
     }
-    // for(unsigned long i = 0;i < array_size; i++)
-    // {   
-    //     mpz_get_str(str1, 10, large[i]);
-    //     printf("large after: %s\n", str1);
-
-    // }
     return 0;
 }
