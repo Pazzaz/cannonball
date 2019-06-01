73662bf8bc0f002586dfe75f178ff96b655ad6cb
Pazzaz <pazzaz.sundqvist@gmail.com>
2019-06-01 13:31:29 +0200

    Skip parametric solutions earlier

--- main2.c
+++ main2.c
@@ -43,6 +43,8 @@ int main(void)
     large = malloc(array_size * sizeof(mpz_t));
     mpz_t *addition;
     addition = malloc(array_size * sizeof(mpz_t));
+    mpz_t *addition2;
+    addition2 = malloc(array_size * sizeof(mpz_t));
 
     // Starting with s = 5
     for(unsigned long i = 0;i < array_size; i++)
@@ -55,24 +57,34 @@ int main(void)
     {   
         mpz_init_set_ui(addition[i], 4*(( (i*i*i)-i)/3));
     }
+
+    for(unsigned long i = 0;i < array_size; i++)
+    {   
+        mpz_init_set_ui(addition2[i], 8*(( (i*i*i)-i)/3));
+    }
     // for(unsigned long i = 0;i < array_size; i++)
     // {   
     //     mpz_get_str(str1, 10, large[i]);
     //     printf("large before: %s\n", str1);
     // }
     printf("haha\n");
-    for(;;mpz_add_ui(Sbig, Sbig, 1)) {
+    for(char mod = 1;;mpz_add_ui(Sbig, Sbig, 1), mod++) {
+        if(mod % 3 == 0) {
+            mpz_add_ui(Sbig, Sbig, 1);
+        }
         mpz_sub_ui(Sm2, Sbig, 2);
-        mpz_mul_ui(Sm2t2, Sm2, 2);
-        mpz_mul_ui(Sm2t8, Sm2, 8);
        
 
         mpz_sub_ui(Sm4, Sbig, 4);
         mpz_mul(Sm4tSm4, Sm4, Sm4);
 
         for (unsigned long j = 2; j < array_size; j++)
-        {
-            mpz_add(large[j], large[j], addition[j]);
+        {   
+            if(mod % 3 == 0) {
+                mpz_add(large[j], large[j], addition2[j]);
+            } else {
+                mpz_add(large[j], large[j], addition[j]);
+            }
 
             mpz_mul(tmp1, large[j], Sm2);
             mpz_add(tmp1, tmp1, Sm4tSm4);
@@ -80,8 +92,10 @@ int main(void)
             if(mpz_perfect_square_p(tmp1)) {
                 mpz_sqrt(tmp2, tmp1);
                 mpz_add(tmp2, tmp2, Sm4);
+
+                mpz_mul_ui(Sm2t2, Sm2, 2);
                 if(mpz_divisible_p(tmp2, Sm2t2)) {
-                    mpz_divexact(tmp2, tmp2, Sm2t8);
+                    mpz_divexact_ui(tmp2, large[j], 8);
                     mpz_get_str(str2, 10, tmp2);
                     mpz_get_str(str3, 10, Sbig);
                     if (mpz_divisible_ui_p(Sm2, 3)) continue;
